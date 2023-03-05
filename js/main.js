9fde3621323048f4b2667022c05eab90d6f22353
T. Lanette <TLPollard4516@gmail.com>
2023-03-05 14:18:07 -0500

    Updated JS
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-// Active Nav Links: Tablet and above
+// Getting JSON DATA
+let planetInfo;
 
+fetch('/assets/data/data.json')
+	.then((response) => response.json())
+	.then((json) => {
+		planetInfo = json;
+		console.log(planetInfo);
+	});
 
+const updatePlanetInfo = (planet) => {
+	const planetLink = document.querySelector('nav-link');
+	const planetName = document.querySelector('.planet-title');
+	const planetImage = document.querySelector('.planet-image');
+	const geologyImage = document.querySelector('.geology-image');
+	const planetDesc = document.querySelector('.planet-text');
+	const source = document.querySelector('.source-link');
+
+	//Buttons Tablet
+	const overviewBtn = document.querySelector('.btn-overview');
+	const structureBtn = document.querySelector('.btn-structure');
+	const geologyBtn = document.querySelector('.btn-surface');
+
+	// Mobile Buttons
+	const overviewBtnMobile = document.querySelector('.btn-overview--mobile');
+	const structureBtnMobile = document.querySelector('.btn-structure--mobile');
+	const geologyBtnMobile = document.querySelector('.btn-surface--mobile');
+
+	planetName.innerText = planetData.name;
+	planetDesc.innerText = planetData.overview.content;
+	source.href = planet.overview.source;
+	planetImage.src = planet.images.planet;
+};
