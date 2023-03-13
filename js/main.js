5e9e68de859425fa803f34fbf0b6911f179abfeb
T. Lanette <TLPollard4516@gmail.com>
2023-03-12 21:28:37 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
-// Get Planet Data
-let planetsInfo;
+// Variables
+const main = document.querySelector('.main');
+const planetLinks = document.querySelectorAll('.nav-link');
+const planetName = document.querySelector('#main-title');
+const planetImage = document.querySelector('.planet-image');
+const geologyImage = document.querySelector('.geology-image');
+const planetContent = document.querySelector('.planet-info--text');
+const source = document.querySelector('.source-link');
+const vw = window.innerWidth;
 
+// Buttons
+const overviewBtn = document.querySelector('#overviewTablet');
+const structureBtn = document.querySelector('#structureTablet');
+const geologyBtn = document.querySelector('#surfaceTablet');
+const overviewBtnMobile = document.querySelector('#overviewMobile');
+const structureBtnMobile = document.querySelector('#structureMobile');
+const geologyBtnMobile = document.querySelector('#surfaceMobile');
+const contentButtons = document.querySelectorAll('.desc-buttons');
+
+let currentPlanet = document.body.id;
+let currentState = 'overview';
+
+// Get Planet Data
+let data;
 const fetchData = () => {
 	fetch('/assets/data/data.json')
 		.then((res) => res.json())
-		.then((data) => {
-			planetsInfo = data;
-			console.log(planetsInfo);
+		.then((json) => {
+			data = json;
+			console.log(data);
 		})
 		.catch((error) => {
 			console.log(`Error Fetching Data : ${error}`);
 		});
 };
-
 fetchData();
 
-// Variables
-const planetLinks = document.querySelectorAll('.nav-link');
-const planetItem = document.querySelectorAll('.nav-item');
-const planetImage = document.querySelector('.planet-image');
-const geologyImage = document.querySelector('.geology-image');
-const planetContent = document.querySelector('.planet-info');
-const source = document.querySelector('.source-link');
-const vw = window.innerWidth;
 
-// Buttons
-const overviewBtn = document.querySelector('.btn-overview');
-const structureBtn = document.querySelector('.btn-structure');
-const geologyBtn = document.querySelector('.btn-surface');
-const overviewBtnMobile = document.querySelector('.btn-overview--mobile');
-const structureBtnMobile = document.querySelector('.btn-structure--mobile');
-const geologyBtnMobile = document.querySelector('.btn-surface--mobile');
-
-let currentPlanet = document.getElementById('#mercury');
-
-const activePlanet = (planetLinks) => {
-	
-};
